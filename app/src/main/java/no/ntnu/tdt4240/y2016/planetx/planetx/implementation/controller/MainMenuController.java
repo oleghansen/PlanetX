93aa5201ef877034964c0be42258af9b99df97ad
Ole <olehansen92@gmail.com>
2016-03-31 12:38:31 +0200

    MainMenuController, dialog for selecting map

--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/controller/MainMenuController.java
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/controller/MainMenuController.java
@@ -1,11 +1,31 @@
 package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.controller;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import no.ntnu.tdt4240.y2016.planetx.planetx.R;
 import no.ntnu.tdt4240.y2016.planetx.planetx.framework.AppMenu;
 
 /**
  * Created by Ole on 31.03.2016.
  */
-public class MainMenuController extends AppMenu {
+public class MainMenuController {
+    private AppMenu context;
 
+    public MainMenuController(AppMenu context){
+        this.context = context;
+    }
 
+    public void selectMapDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(context.getString(R.string.choose_map))
+                .setCancelable(true)
+                .setPositiveButton("Start", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Start game
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
 }
--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/view/MenuActivity.java
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/view/MenuActivity.java
@@ -1,17 +1,35 @@
 package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import no.ntnu.tdt4240.y2016.planetx.planetx.R;
 import no.ntnu.tdt4240.y2016.planetx.planetx.framework.AppMenu;
+import no.ntnu.tdt4240.y2016.planetx.planetx.implementation.controller.MainMenuController;
 
 /**
  * Created by Ole on 31.03.2016.
  */
 public class MenuActivity extends AppMenu {
+
+    private MainMenuController menuController;
+
+    private Button startGameButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        menuController = new MainMenuController(this);
+
+        startGameButton = (Button)findViewById(R.id.btn_startGame);
+
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               menuController.selectMapDialog();
+            }
+        });
     }
 }
