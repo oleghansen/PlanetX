cc6cc2cd3f89f58c49e2bf2f673e26beebe32a16
Ole <olehansen92@gmail.com>
2016-03-31 11:24:30 +0200

    Added Spaceship and Weapon model

--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/Model.java
+++
@@ -1,7 +0,0 @@
-package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.model;
-
-/**
- * Created by Ole on 31.03.2016.
- */
-public class Model {
-}
---
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/Weapon.java
@@ -0,0 +1,16 @@
+package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.model;
+
+/**
+ * Created by Ole on 31.03.2016.
+ */
+public class Weapon {
+
+    private double damage;
+    private String name, description;
+
+    public Weapon(double damage, String name, String description){
+        this.damage = damage;
+        this.name = name;
+        this.description = name;
+    }
+}
