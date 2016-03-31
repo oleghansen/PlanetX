f16cee2494f5b0b08b6905b5e8a6bc62493eb342
Ole <olehansen92@gmail.com>
2016-03-31 12:08:27 +0200

    Created loading activity, redirecting to main menu after loading.

---
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/controller/MainMenuController.java
@@ -0,0 +1,11 @@
+package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.controller;
+
+import no.ntnu.tdt4240.y2016.planetx.planetx.framework.AppMenu;
+
+/**
+ * Created by Ole on 31.03.2016.
+ */
+public class MainMenuController extends AppMenu {
+
+
+}
