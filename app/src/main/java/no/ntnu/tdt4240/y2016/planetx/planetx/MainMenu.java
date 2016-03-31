2291875478dd737911dbfd705f371792f34d76eb
Anders Riise Mæhlum <anderrma@stud.ntnu.no>
2016-03-31 11:01:18 +0200

    Adding project

--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/MainMenu.java
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/MainMenu.java
@@ -13,7 +13,7 @@ public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //test
+        // ehi ehi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
