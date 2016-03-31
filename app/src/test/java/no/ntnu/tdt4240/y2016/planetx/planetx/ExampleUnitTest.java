b2dcf7ae18e8f7462eeee41b832cd0b519c75a0c
Anders Riise Mæhlum <anderrma@stud.ntnu.no>
2016-03-31 10:45:03 +0200

    adding initial project

---
+++ app/src/test/java/no/ntnu/tdt4240/y2016/planetx/planetx/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package no.ntnu.tdt4240.y2016.planetx.planetx;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
