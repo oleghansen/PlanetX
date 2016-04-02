6047e1b5b5bd2c8209ac075d9e0ae6e22dda3a08
Ole <olehansen92@gmail.com>
2016-04-02 13:51:36 +0200

    Map selection working. Not connected it to model yet..

--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/controller/MainMenuController.java
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/controller/MainMenuController.java
@@ -2,11 +2,17 @@ package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.controller;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import no.ntnu.tdt4240.y2016.planetx.planetx.R;
 import no.ntnu.tdt4240.y2016.planetx.planetx.framework.AppMenu;
+import no.ntnu.tdt4240.y2016.planetx.planetx.implementation.view.MenuActivity;
 
 /**
  * Created by Ole on 31.03.2016.
@@ -14,13 +20,14 @@ import no.ntnu.tdt4240.y2016.planetx.planetx.framework.AppMenu;
 public class MainMenuController {
     private AppMenu context;
     private ListView mapListView;
+
     public MainMenuController(AppMenu context){
         this.context = context;
     }
 
     public void selectMapDialog(){
         mapListView = new ListView(context);
-        String[] mapNames = {"Map1", "Map2", "Map3"}; //Temporary for testing
+        String[] mapNames = {"Canis Major", "Sagittarius", "Draco"}; //Temporary for testing
         ArrayAdapter<String> adapter = new ArrayAdapter<>(context, R.layout.maplist_item, R.id.mapName_textView, mapNames);
         mapListView.setAdapter(adapter);
 
@@ -32,6 +39,16 @@ public class MainMenuController {
                         // Start game
                     }
                 }).setView(mapListView);
+
+        mapListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ViewGroup vg = (ViewGroup) view;
+                TextView mapName = (TextView) vg.findViewById(R.id.mapName_textView);
+                Toast.makeText(context, "'"+ mapName.getText().toString() + "' selected.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         AlertDialog alert = builder.create();
         alert.show();
     }
