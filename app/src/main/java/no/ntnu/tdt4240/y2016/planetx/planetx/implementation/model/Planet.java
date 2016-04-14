cbc263fb346a3389f950727937b7c3709522fbc7
Daniel-Rei <Daniel_reinholdt@hotmail.com>
2016-04-14 10:47:21 +0200

    Commit for mergs

--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/Planet.java
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/Planet.java
@@ -1,7 +1,10 @@
 package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.model;
 
+import android.media.Image;
+
 /**
  * Created by Ole on 02.04.2016.
  */
-public class Planet {
+public class Planet extends SpaceObstacle {
+
 }
--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/SpaceObstacle.java
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/SpaceObstacle.java
@@ -3,5 +3,15 @@ package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.model;
 /**
  * Created by Ole on 02.04.2016.
  */
-public class SpaceObstacle {
+public abstract class SpaceObstacle extends SpaceEntity{
+    private double gravity;
+
+    public void setGravity(double x){
+        this.gravity = x;
+    }
+    public double getGravity(){
+        return this.gravity;
+    }
+
 }
+
