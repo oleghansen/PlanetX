5eff7852ea1b86d7e18b474e254f358deae55d3a
Daniel-Rei <Daniel_reinholdt@hotmail.com>
2016-04-15 13:02:23 +0200

    commit

--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/SpaceEntity.java
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/SpaceEntity.java
@@ -1,8 +1,36 @@
 package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.model;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
 /**
  * Created by Ole on 02.04.2016.
  */
-public abstract class SpaceEntity {
+public abstract class SpaceEntity extends ImageView {
+
+    public SpaceEntity(Context context) {
+        super(context);
+    }
+
+    public boolean collidesWith(SpaceEntity object){
+        Rect rc1 = new Rect();
+        this.getDrawingRect(rc1);
+        Rect rc2 = new Rect();
+        object.getDrawingRect(rc2);
+        if (Rect.intersects(rc1, rc2)){
+            Bitmap bm=((BitmapDrawable) this.getDrawable()).getBitmap();
+            Bitmap bmTwo=((BitmapDrawable) object.getDrawable()).getBitmap();
+            return true;
+        }
+        return false;
+    }
+
+    public abstract void collides(SpaceEntity object);
 
 }
--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/SpaceObstacle.java
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/model/SpaceObstacle.java
@@ -1,10 +1,17 @@
 package no.ntnu.tdt4240.y2016.planetx.planetx.implementation.model;
 
+import android.content.Context;
+import android.media.Image;
+
 /**
  * Created by Ole on 02.04.2016.
  */
 public abstract class SpaceObstacle extends SpaceEntity{
     private double gravity;
+    public SpaceObstacle(Context context, double grav){
+        super(context);
+        gravity = grav;
+    }
 
     public void setGravity(double x){
         this.gravity = x;
