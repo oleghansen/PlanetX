f2bb908b79e6da828e97c0050a9a687be03728e2
Christian Hunstad <chrhuns@gmail.com>
2016-04-15 10:28:55 +0200

    Commiting before pulling from master

--- app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/controller/GameActivity.java
+++ app/src/main/java/no/ntnu/tdt4240/y2016/planetx/planetx/implementation/controller/GameActivity.java
@@ -15,12 +15,8 @@ import com.google.example.games.basegameutils.BaseGameUtils;
 /**
  * Created by Ole on 11.04.2016.
  */
-public class GameActivity extends AppMenu implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener, View.OnClickListener  {
-    private static int RC_SIGN_IN = 9001;
-    private GoogleApiClient mGoogleApiClient;
-    private boolean mResolvingConnectionFailure = false;
-    private boolean mAutoStartSignInflow = true;
-    private boolean mSignInClicked = false;
+public class GameActivity extends AppMenu   {
+
 
 
     @Override
@@ -28,76 +24,8 @@ public class GameActivity extends AppMenu implements GoogleApiClient.ConnectionC
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.activity_game);
 
-        // Create the Google Api Client with access to the Play Games services
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
-                        // add other APIs and scopes here as needed
-                .build();
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
-        findViewById(R.id.sign_out_button).setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.sign_in_button) {
-            // start the asynchronous sign in flow
-            mSignInClicked = true;
-            mGoogleApiClient.connect();
-        }
-        else if (view.getId() == R.id.sign_out_button) {
-            // sign out.
-            mSignInClicked = false;
-            Games.signOut(mGoogleApiClient);
-
-            // show sign-in button, hide the sign-out button
-            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-        if (mResolvingConnectionFailure) {
-            // Already resolving
-            return;
-        }
-
-        // If the sign in button was clicked or if auto sign-in is enabled,
-        // launch the sign-in flow
-        if (mSignInClicked || mAutoStartSignInflow) {
-            mAutoStartSignInflow = false;
-            mSignInClicked = false;
-            mResolvingConnectionFailure = true;
-
-            // Attempt to resolve the connection failure using BaseGameUtils.
-            // The R.string.signin_other_error value should reference a generic
-            // error string in your strings.xml file, such as "There was
-            // an issue with sign in, please try again later."
-            if (!BaseGameUtils.resolveConnectionFailure(this,
-                    mGoogleApiClient, connectionResult,
-                    RC_SIGN_IN, this.getString(R.string.signin_other_error))) {
-                mResolvingConnectionFailure = false;
-            }
-        }
-
-        // Put code here to display the sign-in button
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-
-        //   show sign-out button, hide the sign-in button
-        findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-        findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
-
-        // (your code here: update UI, enable functionality that depends on sign in, etc)
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
 
     }
+
+
 }
